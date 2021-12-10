900cf2253b796f099aa910bec55dfb10ddc56d9b
Exopteron <exopteron@protonmail.com>
2021-12-10 23:25:13 +1300

    elevator block start

--- src/main/java/com/exomatic/pesart/blocks/PesartBlocksInitializer.java
+++ src/main/java/com/exomatic/pesart/blocks/PesartBlocksInitializer.java
@@ -1,7 +1,14 @@
 package com.exomatic.pesart.blocks;
 
+import com.exomatic.pesart.Reference;
+
+import net.minecraft.block.Block;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 public class PesartBlocksInitializer {
+    public static final Block ELEVATOR_BLOCK = new ElevatorBlock();
     public static void setup() {
-        
+        Registry.register(Registry.BLOCK, new Identifier(Reference.MODID, "elevator_block"), ELEVATOR_BLOCK);
     }
 }
