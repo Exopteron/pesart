114fa3630ac8c7bb47ca7dcf40c71f6da715f62f
Exopteron <exopteron@protonmail.com>
2021-12-10 22:48:50 +1300

    Initial commit

---
+++ src/main/java/com/exoprism/pesart/Pesart.java
@@ -0,0 +1,14 @@
+package com.exoprism.pesart;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Pesart implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger(Reference.MODID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/com/exoprism/pesart/Reference.java
@@ -0,0 +1,5 @@
+package com.exoprism.pesart;
+
+public class Reference {
+    public static final String MODID = "pesart";
+}
