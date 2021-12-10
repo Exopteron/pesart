8d34b63fcdbbd4f864f0d0d2a65b4b4e39b48926
Exopteron <exopteron@protonmail.com>
2021-12-10 23:00:38 +1300

    forprism

--- src/main/java/com/exomatic/pesart/network/PesartNetworkInitializer.java
+++ src/main/java/com/exomatic/pesart/network/PesartNetworkInitializer.java
@@ -3,6 +3,10 @@ package com.exomatic.pesart.network;
 public class PesartNetworkInitializer {
     private static int packetID = 0;
     public static void setup() {
-        
+        /*
+            for prism:
+            if you want to register a packet, do 
+            ExoNetworkManager.INSTANCE.registerPacket(packetID++, (class that implements IExoPacket).class);
+        */
     }
 }
