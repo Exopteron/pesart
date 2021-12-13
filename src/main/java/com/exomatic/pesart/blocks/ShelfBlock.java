160c9b6899f6e00b957099ae352ba8157bacab1f
Exopteron <exopteron@protonmail.com>
2021-12-14 02:35:41 +1300

    sounds, elevator texture

--- src/main/java/com/exomatic/pesart/blocks/ShelfBlock.java
+++ src/main/java/com/exomatic/pesart/blocks/ShelfBlock.java
@@ -13,10 +13,13 @@ import net.minecraft.block.Material;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.TransparentBlock;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -32,48 +35,66 @@ public class ShelfBlock extends TransparentBlock implements BlockEntityProvider 
     public ShelfBlock() {
         super(FabricBlockSettings.of(Material.WOOD).nonOpaque().requiresTool().strength(1.0F));
     }
+
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 4.0, 16.0);
     }
+
     @Override
     public BlockEntity createBlockEntity(BlockPos var1, BlockState var2) {
         return new ShelfBlockEntity(var1, var2);
     }
+
     @Override
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState,
+            WorldAccess world, BlockPos pos, BlockPos neighborPos) {
         if (direction == Direction.DOWN && !state.canPlaceAt(world, pos)) {
             return Blocks.AIR.getDefaultState();
         }
         return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
+
     @Override
     public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
         BlockPos blockPos = pos.down();
-        return AbstractPressurePlateBlock.hasTopRim(world, blockPos) || AbstractPressurePlateBlock.sideCoversSmallSquare(world, blockPos, Direction.UP);
+        return AbstractPressurePlateBlock.hasTopRim(world, blockPos)
+                || AbstractPressurePlateBlock.sideCoversSmallSquare(world, blockPos, Direction.UP);
     }
+
     // not sure how to do this yet.
     @Override
     public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
         if (!world.isClient) {
             BlockEntity blockEntity = world.getBlockEntity(pos);
             if (blockEntity != null && blockEntity instanceof ShelfBlockEntity shelfBlockEntity) {
-                world.spawnEntity(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), shelfBlockEntity.getHeldItem()));
+                world.spawnEntity(
+                        new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), shelfBlockEntity.getHeldItem()));
+                shelfBlockEntity.setHeldItem(ItemStack.EMPTY);
             }
         }
         super.onBreak(world, pos, state, player);
     }
+    
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand,
             BlockHitResult hit) {
         if (world.isClient) {
+            BlockEntity blockEntity = world.getBlockEntity(pos);
+            if (blockEntity != null && blockEntity instanceof ShelfBlockEntity shelfBlockEntity) {
             if (player.isSneaking()) {
-                BlockEntity blockEntity = world.getBlockEntity(pos);
-                if (blockEntity != null && blockEntity instanceof ShelfBlockEntity shelfBlockEntity) {
                     MinecraftClient mc = MinecraftClient.getInstance();
                     mc.setScreen(new ShelfConfigGui.ShelfConfigScreen(new ShelfConfigGui(shelfBlockEntity)));
+            } else {
+                if (player.getInventory().getMainHandStack().isEmpty()) {
+                    if (!shelfBlockEntity.getHeldItem().isEmpty()) {
+                        player.playSound(SoundEvents.ENTITY_ITEM_FRAME_REMOVE_ITEM, SoundCategory.BLOCKS, 1, 1);
+                    }
+                } else {
+                    player.playSound(SoundEvents.ENTITY_ITEM_FRAME_ADD_ITEM, SoundCategory.BLOCKS, 1, 1);
                 }
             }
+        }
             return ActionResult.SUCCESS;
         }
         BlockEntity blockEntity = world.getBlockEntity(pos);
@@ -91,5 +112,9 @@ public class ShelfBlock extends TransparentBlock implements BlockEntityProvider 
             }
         }
         return ActionResult.FAIL;
+    }
+    @Override
+    public PistonBehavior getPistonBehavior(BlockState state) {
+        return PistonBehavior.DESTROY;
     }
 }
